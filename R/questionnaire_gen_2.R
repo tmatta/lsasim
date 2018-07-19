2a10d87802bbceb356a0a33780ec8e7ebf757536
Waldir Leoncio <waldir.leoncio@gmail.com>
2018-07-19 10:25:52 +0200

    questionnaire_gen_2() now accepts family = "gaussian" as a parameter.

--- R/questionnaire_gen_2.R
+++ R/questionnaire_gen_2.R
@@ -14,6 +14,7 @@
 #' @param c_sd is a vector of population standard deviations for each continuous variable.
 #' @param theta if \code{TRUE} will labeled the first continuous variable 'theta'.
 #' @param family distribution family, can be NULL, 'multinomial' or 'binomial'.
+#' @param vcov_yxw covariance matrix of the latent trait (Y), X and W.
 #'
 #' @section Details:
 #' \code{cat_prop} is a list where \code{length(cat_prop)} is the number of
@@ -39,13 +40,19 @@
 #'
 #'
 #' @examples
-#' questionnaire_gen(n = 10, cat_prop = list(c(1), c(.25, .6, 1)),
+#' # Using polychoric correlations
+#' questionnaire_gen_2(n = 10, cat_prop = list(c(1), c(.25, .6, 1)),
 #'                   cor_matrix = matrix(c(1, .6, .6, 1), nrow = 2),
 #'                   c_mean = 2, c_sd = 1.5, theta = TRUE)
 #'
+#' # Using the multinomial distribution
+#' cov_matrix <- cov_gen(n_fac = 2, n_ind = 3)$vcov_yxw
+#' questionnaire_gen_2(n = 10, cov_matrix = cov_matrix)
+#'
 #' @export
-questionnaire_gen_2 <- function(n_obs, cat_prop, cor_matrix, c_mean = NULL,
-                                c_sd = NULL, theta = FALSE, family = NULL){
+questionnaire_gen_2 <- function(n_obs, cat_prop = NULL, cor_matrix = NULL,
+                                c_mean = NULL, c_sd = NULL, theta = FALSE,
+                                family = "gaussian", cov_matrix = NULL){
 
   #--- Load functions -----------------------------------------------------------#
   #--- generates data from num_x independent standard normals
@@ -55,120 +62,128 @@ questionnaire_gen_2 <- function(n_obs, cat_prop, cor_matrix, c_mean = NULL,
     return(uncor_mat)
   }
   #------------------------------------------------------------------------------#
-  #TODO: add check for existence of cat_prop or cor_matrix
-  #TODO: if those are NULL and family is set, use the following:
-  #--- Generate background data:
-  # bg_dat_full <- data.frame(mvtnorm::rmvnorm(n, mean = rep(0, 38), sigma = vcov_yxw))
-  # colnames(bg_dat_full) <- gen_var_names
-  # bg_dat_full$z <- cut(bg_dat_full[, "w"], c(-Inf, qnorm(pr_grp_1), Inf), labels = c("0", "1"))
-  # bg_dat_full$z <- as.numeric(bg_dat_full$z) - 1
-  # bg_dat_full$w <- NULL
-
-  #--- Generate uncorrelated standard normals
-  n_vars <- length(cat_prop)
-  uncor_dat <- mvsn(n = n_obs, num_x = n_vars)
-
-  #--- Correlate data
-  chol_cor_matrix <- chol(cor_matrix)
-  cor_dat <- uncor_dat %*% chol_cor_matrix
-
-  #--- Number of categories for each variable in cor_dat
-  n_cats <- unlist(lapply(cat_prop, function(x) length(x)))
-
-  #--- Scale continuous variables ---------------------------------------------#
-  if (any(n_cats == 1)){
-    #--- which variables are continuous
-    continuous_var <- which(n_cats == 1)
-
-    #--- if they are left null, the rescaling is neutralized
-    if (is.null(c_mean)) c_mean <- rep(0, length(continuous_var))
-    if (is.null(c_sd)) c_sd <- rep(1, length(continuous_var))
-
-    for (i in 1: length(continuous_var)){
-      cor_dat[, continuous_var[i]] <- cor_dat[, continuous_var[i]] * c_sd[i] + c_mean[i]
+  if (is.null(cat_prop) | is.null(cor_matrix)) {
+    message("Using ", family, " distribution")
+    #TODO: if those are NULL and family is set, use the following:
+    if (family == "gaussian") {
+      raw_data <- mvtnorm::rmvnorm(n_obs, mean = rep(0, nrow(cov_matrix)), sigma = cov_matrix)
     }
-
-  }
-
-  #--- Coarsen discrete variables ---------------------------------------------#
-  if (any(n_cats > 1)){
-
-    #--- which variables are discrete
-    discrete_var <- which(n_cats > 1)
-
-    #--- Replace cumulative pr = 1 with .999999 for computation
-    cat_prop_2 <- lapply(cat_prop[discrete_var], function(x) ifelse(x == 1, (1 - 1e-7), x))
-
-    #--- Find thresholds assuming standard normal
-    var_thresholds <- lapply(cat_prop_2, function(x) qnorm(x, 0, 1))
-
-    #--- Coarsen variables based on thresholds
-    discrete_dat <- cor_dat
-
-    #--- place an upper limit on the z score so that it will not exceed the upper threshold
-    discrete_dat[discrete_dat[, discrete_var] > 5.6]  <- 5.6
-
-    for (i in 1:length(discrete_var)) {
-      for (j in rev(1:length(var_thresholds[[i]]))) {
-        discrete_dat[which(cor_dat[, discrete_var[i]] <= var_thresholds[[i]][j]), discrete_var[i]] <- j
-      }
-    }
+    bg_dat_full <- data.frame(subject = 1:nrow(raw_data), raw_data)
+    gen_var_names <- c("subject", "y", paste0("x", seq(nrow(cov_matrix) - 2)), "w")
+    gen_var_names <- c("subject", "theta", paste0("q", seq(nrow(cov_matrix) - 2)), "w")  # TODO: correct?
+    colnames(bg_dat_full) <- gen_var_names
+    pr_grp_1 <- .66 # TODO: best way to get this?
+    bg_dat_full$z <- cut(bg_dat_full$w, c(-Inf, qnorm(pr_grp_1), Inf), labels = 0:1)
+    bg_dat_full$z <- as.numeric(bg_dat_full$z) - 1
+    bg_dat_full$w <- NULL
+    discrete_df <- bg_dat_full
   } else {
-    discrete_dat <- cor_dat
-  }
+    message("Using cumulative proportions and correlation matrix")
+    #--- Generate uncorrelated standard normals
+    n_vars <- length(cat_prop)
+    uncor_dat <- mvsn(n = n_obs, num_x = n_vars)
 
-  discrete_df <- data.frame(discrete_dat)
+    #--- Correlate data
+    chol_cor_matrix <- chol(cor_matrix)
+    cor_dat <- uncor_dat %*% chol_cor_matrix
 
-  #--- Create factor variables for all discrete variables.
-  if (length(which(n_cats > 1)) > 1){
-    fac_var_list <- lapply(discrete_df[, discrete_var], factor)
-    fac_var_mat <- do.call(cbind.data.frame, fac_var_list)
-    discrete_df[, discrete_var] <- fac_var_mat
-  }
+    #--- Number of categories for each variable in cor_dat
+    n_cats <- unlist(lapply(cat_prop, function(x) length(x)))
 
-  if (length(which(n_cats > 1)) == 1){
-    discrete_df[, discrete_var] <- as.factor(discrete_df[, discrete_var])
-  }
+    #--- Scale continuous variables ---------------------------------------------#
+    if (any(n_cats == 1)){
+      #--- which variables are continuous
+      continuous_var <- which(n_cats == 1)
 
+      #--- if they are left null, the rescaling is neutralized
+      if (is.null(c_mean)) c_mean <- rep(0, length(continuous_var))
+      if (is.null(c_sd)) c_sd <- rep(1, length(continuous_var))
 
-  #--- Name variables ---------------------------------------------------------#
+      for (i in 1: length(continuous_var)){
+        cor_dat[, continuous_var[i]] <- cor_dat[, continuous_var[i]] * c_sd[i] + c_mean[i]
+      }
 
-  #--- If the list cat_prop is not labeled, give it labels q1, ..., qX
-  if (is.null(names(cat_prop))) {
-
-    if (all(vapply(discrete_df, is.factor, c(is.factor = FALSE))) & theta == TRUE){
-      stop("Cannot assign theta, all data are discrete", call. = FALSE)
     }
 
-    if (theta == TRUE){
+    #--- Coarsen discrete variables ---------------------------------------------#
+    if (any(n_cats > 1)){
 
-      #  Call first non-integer variable theta.
-      colnames(discrete_df)[which(!(vapply(discrete_df, is.factor, c(is.factor = FALSE))))[1]] <- "theta"
-      xtra_cols <- paste0("q", 1:(ncol(discrete_df)-1) )
-      colnames(discrete_df)[which(colnames(discrete_df) != "theta")] <- xtra_cols
-      discrete_df$subject <- 1:nrow(discrete_df)
-      discrete_df <- discrete_df[, c("subject", "theta", xtra_cols)]
+      #--- which variables are discrete
+      discrete_var <- which(n_cats > 1)
+
+      #--- Replace cumulative pr = 1 with .999999 for computation
+      cat_prop_2 <- lapply(cat_prop[discrete_var], function(x) ifelse(x == 1, (1 - 1e-7), x))
+
+      #--- Find thresholds assuming standard normal
+      var_thresholds <- lapply(cat_prop_2, function(x) qnorm(x, 0, 1))
+
+      #--- Coarsen variables based on thresholds
+      discrete_dat <- cor_dat
+
+      #--- place an upper limit on the z score so that it will not exceed the upper threshold
+      discrete_dat[discrete_dat[, discrete_var] > 5.6]  <- 5.6
+
+      for (i in 1:length(discrete_var)) {
+        for (j in rev(1:length(var_thresholds[[i]]))) {
+          discrete_dat[which(cor_dat[, discrete_var[i]] <= var_thresholds[[i]][j]), discrete_var[i]] <- j
+        }
+      }
+    } else {
+      discrete_dat <- cor_dat
+    }
+
+    discrete_df <- data.frame(discrete_dat)
+
+    #--- Create factor variables for all discrete variables.
+    if (length(which(n_cats > 1)) > 1){
+      fac_var_list <- lapply(discrete_df[, discrete_var], factor)
+      fac_var_mat <- do.call(cbind.data.frame, fac_var_list)
+      discrete_df[, discrete_var] <- fac_var_mat
+    }
+
+    if (length(which(n_cats > 1)) == 1){
+      discrete_df[, discrete_var] <- as.factor(discrete_df[, discrete_var])
+    }
+
+
+    #--- Name variables ---------------------------------------------------------#
+
+    #--- If the list cat_prop is not labeled, give it labels q1, ..., qX
+    if (is.null(names(cat_prop))) {
+
+      if (all(vapply(discrete_df, is.factor, c(is.factor = FALSE))) & theta == TRUE){
+        stop("Cannot assign theta, all data are discrete", call. = FALSE)
+      }
+
+      if (theta == TRUE){
+
+        #  Call first non-integer variable theta.
+        colnames(discrete_df)[which(!(vapply(discrete_df, is.factor, c(is.factor = FALSE))))[1]] <- "theta"
+        xtra_cols <- paste0("q", 1:(ncol(discrete_df)-1) )
+        colnames(discrete_df)[which(colnames(discrete_df) != "theta")] <- xtra_cols
+        discrete_df$subject <- 1:nrow(discrete_df)
+        discrete_df <- discrete_df[, c("subject", "theta", xtra_cols)]
+
+      } else {
+
+        xtra_cols <- paste0("q", 1:(ncol(discrete_df)) )
+        colnames(discrete_df) <- xtra_cols
+        discrete_df$subject <- 1:nrow(discrete_df)
+        discrete_df <- discrete_df[, c("subject", xtra_cols)]
+
+      }
 
     } else {
 
-      xtra_cols <- paste0("q", 1:(ncol(discrete_df)) )
-      colnames(discrete_df) <- xtra_cols
+      df_named <-  names(cat_prop)[which(names(cat_prop) != "NA")]
+      colnames(discrete_df)[which(names(cat_prop) != "NA")] <- df_named
+      df_unnamed <- paste0("p", seq_along(names(cat_prop))[is.na(names(cat_prop))])
+      colnames(discrete_df)[is.na(names(cat_prop))] <- df_unnamed
+      df_names <- colnames(discrete_df)
       discrete_df$subject <- 1:nrow(discrete_df)
-      discrete_df <- discrete_df[, c("subject", xtra_cols)]
+      discrete_df <- discrete_df[, c("subject", df_names)]
 
     }
-
-  } else {
-
-    df_named <-  names(cat_prop)[which(names(cat_prop) != "NA")]
-    colnames(discrete_df)[which(names(cat_prop) != "NA")] <- df_named
-    df_unnamed <- paste0("p", seq_along(names(cat_prop))[is.na(names(cat_prop))])
-    colnames(discrete_df)[is.na(names(cat_prop))] <- df_unnamed
-    df_names <- colnames(discrete_df)
-    discrete_df$subject <- 1:nrow(discrete_df)
-    discrete_df <- discrete_df[, c("subject", df_names)]
-
   }
-
   return(discrete_df)
 }
