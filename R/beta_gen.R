ee942a71fae324ba934499f41dbb35629b62c409
Waldir Leoncio <waldir.leoncio@gmail.com>
2018-08-31 08:56:36 +0200

    Added example to beta_gen()
    Dry run of using the output of questionnaire_gen() as input to beta_gen()

--- R/beta_gen.R
+++ R/beta_gen.R
@@ -5,6 +5,15 @@
 #' @param wcol_Phi vector with column numbers of the W variables in Phi
 #' @param pr_grp_1 scalar or list of proportions of the first group
 #' @export
+#' @examples
+#' bg <- questionnaire_gen(100, n_X = 2, n_W = c(2, 2), theta = TRUE,
+#'                         full_output = TRUE)
+#' fit.obs <- lm(theta ~ q1 + q2 + q3 + q4, data = test$bg)
+#' prop.gr.1 <- apply(test$bg[c("q3", "q4")], 2, function(x) prop.table(table(x)))[1, ]
+#' fit.exp <- beta_gen(vcov_yfz = test$cov_matrix, Phi = test$cor_matrix,
+#'                     wcol_Phi = 4:5, pr_grp_1 = prop.gr.1)
+#'
+#' cbind(obs = fit.obs$coefficients[2:5], exp = fit.exp$beta_hat)
 beta_gen <- function(vcov_yfz, Phi, wcol_Phi, pr_grp_1) {
   # TODO: change inputs so that background data can be provided
   ncol_Phi <- ncol(Phi)
