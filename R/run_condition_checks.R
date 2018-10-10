704b633dd3d6d0f608f963d8ece90415a2f48dfb
Waldir Leoncio <waldir.leoncio@gmail.com>
2018-10-10 17:12:07 +0200

    Moved condition checks into their own function

---
+++ R/run_condition_checks.R
@@ -0,0 +1,62 @@
+#' Wrapper-function for check_condition
+#'
+#' @param n_cats
+#' @param n_vars
+#' @param n_X
+#' @param n_W
+#' @param theta
+#' @param cat_prop
+#' @param cor_matrix
+#' @param cov_matrix
+#'
+run_condition_checks <- function(n_cats, n_vars, n_X, n_W, theta, cat_prop,
+                                 cor_matrix, cov_matrix, c_mean, c_sd) {
+  check_condition(any(n_cats == 1),
+                  "the number of categories in n_W must all be greater than 1")
+  check_condition(n_vars != n_X + n_W + theta,
+                  "n_vars must equal n_X + n_W + theta")
+  check_condition(!is.null(cat_prop) & length(cat_prop) != ncol(cor_matrix),
+                  "length(cat_prop) cannot be different from ncol(cor_matrix)")
+  check_condition(!is.null(cat_prop) & length(cat_prop) != ncol(cov_matrix),
+                  "length(cat_prop) cannot be different from ncol(cov_matrix)")
+  check_condition(!is.null(cat_prop) & (!is.null(n_X) | !is.null(n_W)),
+                  "cat_prop was provided, so n_X and n_W were ignored", FALSE)
+  check_condition(ncol(cov_matrix) > 0 & ncol(cor_matrix) > 0,
+                  "Only one matrix (cov_matrix or cor_matrix) can be provided")
+  check_condition(n_X + n_W + theta > ncol(cov_matrix),
+                  "n_X + n_W + theta must not exceed ncol(cov_matrix)")
+  check_condition(n_X + n_W + theta > ncol(cor_matrix),
+                  "n_X + n_W + theta must not exceed ncol(cor_matrix)")
+  check_condition(n_vars > ncol(cov_matrix),
+                  "n_vars must not exceed ncol(cov_matrix)")
+  check_condition(n_vars > ncol(cor_matrix),
+                  "n_vars must not exceed ncol(cor_matrix)")
+  check_condition(!is.null(cat_prop) & !is.null(n_vars) &
+                    length(cat_prop) != n_vars,
+                  "n_vars must be NULL or equal to length(cat_prop)")
+  check_condition(any(cor_matrix > 1),
+                  "Improper correlation matrix")
+  check_condition(any(sapply(lapply(cat_prop, diff), function(x) any(x < 0))),
+                  "The elements of cat_prop must be non-decreasing")
+  check_condition(any(sapply(cat_prop, function(x) any(x > 1))),
+                  "cat_prop must not contain values above 1")
+  check_condition(any(sapply(cat_prop, max) != 1),
+                  "last value of each element of cat_prop must be 1")
+  check_condition(length(c_mean) > n_X + theta,
+                  "length(c_mean) cannot be larger than n_X + theta")
+  check_condition(length(c_mean) > 1 & length(c_mean) != n_X + theta,
+                  "c_mean recycled to fit all continuous variables", FALSE)
+  check_condition(length(c_sd) > n_X + theta,
+                  "length(c_sd) cannot be larger than n_X + theta")
+  check_condition(length(c_sd) > 1 & length(c_sd) != n_X + theta,
+                  "c_sd  recycled to fit all continuous variables", FALSE)
+  check_condition(any(c_sd < 0),
+                  "c_sd may not contain negative elements")
+  if (!is.null(cor_matrix))
+    check_condition(!isSymmetric(cor_matrix), "cor_matrix is not symmetric")
+  if (!is.null(cov_matrix))
+    check_condition(!isSymmetric(cov_matrix), "cov_matrix is not symmetric")
+  if (!is.null(cat_prop))
+    check_condition(theta & (cat_prop[[1]][1] != 1),
+                    "theta == TRUE, so the first element of cat_prop must be 1")
+}
