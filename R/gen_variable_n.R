c4340317403cbaf43c5b403f9e0eddcbd3f7958f
Waldir Leoncio <waldir.leoncio@gmail.com>
2018-10-24 16:34:06 +0200

    Fixed calculation of n_tot in some particular cases

--- R/gen_variable_n.R
+++ R/gen_variable_n.R
@@ -22,8 +22,13 @@ gen_variable_n <- function(n_vars, n_X, n_W, theta = FALSE) {
     return(k)
   }
   if (is.null(n_vars)) {
-    n_vars <- rzeropois(lambda = 4) + ifelse(is.null(n_W), 0, n_W) +
-      ifelse(is.null(n_X), 0, n_X) + theta
+    if (is.null(n_W) | is.null(n_X)) {
+      n_vars <- rzeropois(lambda = 4) + ifelse(is.null(n_W), 0, n_W) +
+        ifelse(is.null(n_X), 0, n_X) + theta
+    } else {
+      n_vars <- n_W + n_X + theta
+
+    }
   }
   if (is.null(n_X) & is.null(n_W)) {
     n_X <- rbinom(n = 1, size = n_vars, prob = .2)
