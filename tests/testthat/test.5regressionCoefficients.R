a2464a191fbb3404115a51fbc43bbc67a587cd6c
Unknown <waldir.leoncio@gmail.com>
2018-12-28 12:41:11 +0100

    Added test cases for regression coefficients
    Probably too many. Might cut down later.

--- tests/testthat/test.5regressionCoefficients.R
+++ tests/testthat/test.5regressionCoefficients.R
@@ -9,6 +9,9 @@ df_1X <- q_gen_wrap(1)
 df_2X <- q_gen_wrap(2)
 df_1W <- q_gen_wrap(0, list(2))
 df_2W <- q_gen_wrap(0, list(2, 2))
+df_1Z <- q_gen_wrap(0, list(3))
+df_1X1W <- q_gen_wrap(1, list(2))
+df_2X2W2Z <- q_gen_wrap(2, list(2, 2, 3, 4))
 
 # Generating replications -------------------------------------------------
 calcPctIn <- function(data) {
@@ -20,13 +23,19 @@ calcPctIn <- function(data) {
   avg_coverage <- apply(pct_coverage, 1, mean)
   return(avg_coverage)
 }
-# pct_df_1X <- calcPctIn(df_1X)
-# pct_df_2X <- calcPctIn(df_2X)
-# pct_df_1W <- calcPctIn(df_1W)
-# pct_df_2W <- calcPctIn(df_2W)
+pct_df_1X <- calcPctIn(df_1X)
+pct_df_2X <- calcPctIn(df_2X)
+pct_df_1W <- calcPctIn(df_1W)
+pct_df_2W <- calcPctIn(df_2W)
+pct_df_1Z <- calcPctIn(df_1Z)
+pct_df_1X1W <- calcPctIn(df_1X1W)
+pct_df_2X2W2Z <- calcPctIn(df_2X2W2Z)
 
 # Testing for near-equality -----------------------------------------------
-# test_that("n_X = 1", expect_gte(min(pct_df_1X), 0.5))
-# test_that("n_X = 2", expect_gte(min(pct_df_2X), 0.5))
-# test_that("n_W = 1", expect_gte(min(pct_df_1W), 0.5))
-# test_that("n_W = 2", expect_gte(min(pct_df_2W), 0.5))
+test_that("1 X", expect_gte(min(pct_df_1X), 0.9))
+test_that("2 X", expect_gte(min(pct_df_2X), 0.9))
+test_that("1 binomial W", expect_gte(min(pct_df_1W), 0.9))
+test_that("2 binomial W", expect_gte(min(pct_df_2W), 0.9))
+test_that("1 polynomial Z", expect_gte(min(pct_df_1W), 0.9))
+test_that("1 X and 1 W", expect_gte(min(pct_df_2W), 0.9))
+test_that("2 X, 2 W, 2 Z", expect_gte(min(pct_df_2W), 0.9))
