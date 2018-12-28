c752c1405ebe245189dcb0f31cbe6ac8138111d6
Unknown <waldir.leoncio@gmail.com>
2018-12-28 13:19:43 +0100

    Improved speed of regression coefficient test battery

--- tests/testthat/test.5regressionCoefficients.R
+++ tests/testthat/test.5regressionCoefficients.R
@@ -17,7 +17,7 @@ df_2X2W2Z <- q_gen_wrap(2, list(2, 2, 3, 4))
 calcPctIn <- function(data) {
   estimates <- replicate(n    = 10,
                          expr = suppressMessages(beta_gen(data, MC = TRUE,
-                                                          replications = 100,
+                                                          replications = 30,
                                                           analytical = TRUE)))
   pct_coverage <- estimates[, "cov_in_CI", ]
   avg_coverage <- apply(pct_coverage, 1, mean)
