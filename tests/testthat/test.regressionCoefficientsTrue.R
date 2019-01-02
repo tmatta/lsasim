ed47f54f3be5e8b70852927e7507fd9d50384137
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-01-02 17:24:30 +0100

    Minor changes to new regression coefficient test

--- tests/testthat/test.regressionCoefficientsTrue.R
+++ tests/testthat/test.regressionCoefficientsTrue.R
@@ -4,7 +4,7 @@ context("True regression coefficients are sensible")
 # Setup -------------------------------------------------------------------
 rm(list = ls())
 set.seed(2)
-n <- 1e5
+n <- 1e4
 
 mu_x <- 0
 var_x <- 1
@@ -18,7 +18,7 @@ cum_prob <- c(.2, 1)
 mu_z <- diff(cum_prob)
 var_z <- sapply(mu_z, function(x) x * (1 - x))
 
-cov_xy <- .05
+cov_xy <- 0.4
 
 # Generating data ---------------------------------------------------------
 vcov_xy <- matrix(c(var_x, cov_xy, cov_xy, var_y), 2)
@@ -35,8 +35,8 @@ reg_xyz <- lm(x ~ y + z, xyz)
 # Numerical solution
 xyz_given_z0 <- xyz[xyz$z == 0, ]
 xyz_given_z1 <- xyz[xyz$z == 1, ]
-print(identical(xyz[xyz$z == 0, ], xyz[xyz$y < q_y_cum[2], ]))
-print(identical(xyz[xyz$z == 1, ], xyz[xyz$y > q_y_cum[2], ]))
+# print(identical(xyz[xyz$z == 0, ], xyz[xyz$y < q_y_cum[2], ]))
+# print(identical(xyz[xyz$z == 1, ], xyz[xyz$y > q_y_cum[2], ]))
 cov_yz <- mean(xyz_given_z1$y) * mu_z - mu_z * mu_y
 cov_xz <- mean(xyz_given_z1$x) * mu_z - mu_z * mu_x
 
@@ -45,7 +45,7 @@ stnorm_y <- (q_y_cum[2] - mu_y) / sd_y
 inv_mills_ratio <- dnorm(stnorm_y) / (1 - pnorm(stnorm_y))
 
 mu_y_given_y <- mu_y + sd_y * inv_mills_ratio  # truncated normal
-mu_x_given_y <- mu_x + (cov_xy / sd_y) * inv_mills_ratio # mvn
+mu_x_given_y <- mu_x + (cov_xy / sd_y) * inv_mills_ratio # multivariate normal
 
 cov_yz <- mu_y_given_y * mu_z - mu_y * mu_z
 cov_xz <- mu_x_given_y * mu_z - mu_x * mu_z
@@ -54,18 +54,20 @@ cov_xz <- mu_x_given_y * mu_z - mu_x * mu_z
 vcov_xyz <- matrix(c(var_x, cov_xy, cov_xz,
                      cov_xy, var_y, cov_yz,
                      cov_xz, cov_yz, var_z), 3)
+cat("\nTrue covariance Matrix\n")
 print(vcov_xyz)
 
 # Checking regression coefficients ----------------------------------------
-beta_xyz <- solve(vcov_xyz[-1, -1], vcov_xyz[1, -1])
+beta_xyz <- solve(vcov_xyz[-1, -1]) %*% vcov_xyz[1, -1]
 alpha_xyz <- mu_x - crossprod(beta_xyz, c(mu_y, mu_z))
 
 cat("\nObserved and expected regression coefficients\n")
 print(rbind(obs = coef(reg_xyz), exp = c(alpha_xyz, beta_xyz)))
 
-cat("\nStandardized differences:\n")
-std_diff <- (coef(reg_xyz) - c(alpha_xyz, beta_xyz)) / c(alpha_xyz, beta_xyz)
+cat("\nDifferences:\n")
+diff <- coef(reg_xyz) - c(alpha_xyz, beta_xyz)
+print(diff)
 
 test_that("Numerical and analytical solutions are close: binary W", {
-  expect_lte(max(std_diff), 0.01)
+  expect_lte(max(diff), 0.01)
 })
