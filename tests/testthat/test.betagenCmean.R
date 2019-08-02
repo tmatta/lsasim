b2b5161564983b2fe9c8640eeb922614f0729101
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-02 18:00:11 +0200

    Added verbose args + new n

--- tests/testthat/test.betagenCmean.R
+++ tests/testthat/test.betagenCmean.R
@@ -8,17 +8,18 @@
 context("beta_gen() behavior with n_W > 0 and c_means != 0")
 
 # Generating data -------------------------------------------------------------
-qGenWrap <- function(nX, nW, mu = NULL, n = 2e3) {
+qGenWrap <- function(nX, nW, mu = NULL, n = 3e3) {
 	# ad-hoc wrapper for questionnaire_gen()
-	data <- suppressMessages(questionnaire_gen(n_obs       = n,
-						            	                   n_X         = nX,
-                                             n_W         = nW,
-                                             c_mean      = mu,
-                                             theta       = TRUE,
-                                             full_output = TRUE,
-                                             family      = "gaussian"))
+	data <- questionnaire_gen(n_obs       = n,
+                            n_X         = nX,
+                            n_W         = nW,
+                            c_mean      = mu,
+                            theta       = TRUE,
+                            full_output = TRUE,
+                            family      = "gaussian",
+                            verbose = FALSE)
   means <- apply(data$bg[-1], 2, mean)
-  betas <- suppressMessages(beta_gen(data, MC = TRUE))
+  betas <- beta_gen(data, MC = TRUE, verbose = FALSE)
   return(list(data = data, means = means, betas = betas))
 }
 
