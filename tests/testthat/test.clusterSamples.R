1677fffc9f9147057b1c08f33fa09c096f8d3d10
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-05 09:29:58 +0200

    Changed argument defaults + better documentation

--- tests/testthat/test.clusterSamples.R
+++ tests/testthat/test.clusterSamples.R
@@ -1,7 +1,8 @@
 context("Cluster samples")
 
 # Basic dataset
-clusters1 <- cluster_gen(c(3, 2), c(10, 5), n_X = 1, n_W = list(2, 3))
+clusters1 <- cluster_gen(c(3, 2), c(10, 5), n_X = 1, n_W = list(2, 3),
+                         collapse = TRUE)
 message("Data generated")
 print(clusters1)
 message("Means per cluster and variable")
@@ -9,7 +10,7 @@ print(lapply(clusters1, function(x) aggregate(. ~ clusterID, x[-1], mean)))
 
 # Changing means
 clusters2 <- cluster_gen(c(3, 2), 1000, n_X = 1, n_W = list(2, 2),
-                         c_mean = list(1, 3))
+                         c_mean = list(1, 3), collapse = TRUE)
 message("Different means per cluster and variable")
 print(lapply(clusters2, function(x) aggregate(. ~ clusterID, x[-1], mean)))
 
