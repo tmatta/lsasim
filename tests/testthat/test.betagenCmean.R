20ce3815b103a92b75e68df52c0e96c69cc19785
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-07 10:21:38 +0200

    Further simplifications to beta_gen c_mean test

--- tests/testthat/test.betagenCmean.R
+++ tests/testthat/test.betagenCmean.R
@@ -39,82 +39,23 @@ qGenWrap <- function(nX, nW, mu = NULL, n = 300, verbose = FALSE) {
 
 # The numbering logic below is df_abc, where the "abc" contains numbers of n_X amd n_W and for mu
 
-# Zero mean
-df_100  <- qGenWrap(1, 0)
-df_010b <- qGenWrap(0, list(2))
-# df_010t <- qGenWrap(0, list(3))
 df_110b <- qGenWrap(1, list(2))
-# df_110t <- qGenWrap(1, list(3))
-
-# Positive means
-df_101  <- qGenWrap(1, 0, 1)
-df_011b <- qGenWrap(0, list(2), 1)
-# df_011t <- qGenWrap(0, list(3), 1)
-df_111b <- qGenWrap(1, list(2), 1)
-# df_111t <- qGenWrap(1, list(3), 1)
 df_229m <- qGenWrap(2, list(2, 3), 9)
-
-# Negative means
-df_101n  <- qGenWrap(1, 0, -1)
-df_011nb <- qGenWrap(0, list(2), -1)
-# df_011nt <- qGenWrap(0, list(3), -1)
-df_111nb <- qGenWrap(1, list(2), -1)
-# df_111nt <- qGenWrap(1, list(3), -1)
 df_229nm <- qGenWrap(2, list(2, 3), -9)
-
-# Mixed means
-df_101m  <- qGenWrap(1, 0, c(10, -20))
 df_111mb <- qGenWrap(1, list(2), c(20, -30))
-# df_111mt <- qGenWrap(1, list(3), c(30, -40))
 
 # Checking means -------------------------------------------------------------
 test_that("Observed means equal expectations", {
-  # Zero means
-	expect_equivalent(df_100$means,  c(0, 0), tolerance = 0.2)
-  expect_equivalent(df_010b$means, c(0),    tolerance = 0.2)
-  # expect_equivalent(df_010t$means, c(0),    tolerance = 0.2)
   expect_equivalent(df_110b$means, c(0, 0), tolerance = 0.2)
-  # expect_equivalent(df_110t$means, c(0, 0), tolerance = 0.2)
-  # Positive means
-  expect_equivalent(df_101$means,  c(1, 1), tolerance = 0.2)
-  expect_equivalent(df_011b$means, c(1),    tolerance = 0.2)
-  # expect_equivalent(df_011t$means, c(1),    tolerance = 0.2)
   expect_equivalent(df_111b$means, c(1, 1),  tolerance = 0.2)
-  # expect_equivalent(df_111t$means, c(1, 1),  tolerance = 0.2)
-  # Negative means
-  expect_equivalent(df_101n$means,  c(-1, -1), tolerance = 0.2)
-  expect_equivalent(df_011nb$means, c(-1),     tolerance = 0.2)
-  # expect_equivalent(df_011nt$means, c(-1),     tolerance = 0.2)
   expect_equivalent(df_111nb$means, c(-1, -1), tolerance = 0.2)
-  # expect_equivalent(df_111nt$means, c(-1, -1), tolerance = 0.2)
-  # Mixed means
-  expect_equivalent(df_101m$means,  c(10, -20), tolerance = 0.2)
   expect_equivalent(df_111mb$means, c(20, -30),  tolerance = 0.2)
-  # expect_equivalent(df_111mt$means, c(30, -40),  tolerance = 0.2)
 })
 
 # Checking betas --------------------------------------------------------------
 test_that("Estimated true betas are equivalent to MC estimates", {
-  # Zero means
-  expect_gte(min(df_100$avg_coverage),  .5)
-  expect_gte(min(df_010b$avg_coverage), .5)
-  expect_gte(min(df_010t$avg_coverage), .5)
   expect_gte(min(df_110b$avg_coverage), .5)
-  expect_gte(min(df_110t$avg_coverage), .5)
-  # Positive means
-  expect_gte(min(df_101$avg_coverage),  .5)
-  expect_gte(min(df_011b$avg_coverage), .5)
-  expect_gte(min(df_011t$avg_coverage), .5)
   expect_gte(min(df_111b$avg_coverage), .5)
-  expect_gte(min(df_111t$avg_coverage), .5)
-  # Negative means
-  expect_gte(min(df_101n$avg_coverage),  .5)
-  expect_gte(min(df_011nb$avg_coverage), .5)
-  expect_gte(min(df_011nt$avg_coverage), .5)
   expect_gte(min(df_111nb$avg_coverage), .5)
-  expect_gte(min(df_111nt$avg_coverage), .5)
-  # Mixed means
-  expect_gte(min(df_101m$avg_coverage),  .5)
   expect_gte(min(df_111mb$avg_coverage), .5)
-  expect_gte(min(df_111mt$avg_coverage), .5)
 })
