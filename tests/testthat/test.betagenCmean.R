993fe00049f5a880e9217641b54836999763c444
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-08 11:52:50 +0200

    Further reduction to beta_gen unit tests

--- tests/testthat/test.betagenCmean.R
+++ tests/testthat/test.betagenCmean.R
@@ -1,7 +1,7 @@
 context("beta_gen() behavior with n_W > 0 and c_means != 0")
 
 # Generating data -------------------------------------------------------------
-qGenWrap <- function(nX, nW, mu = NULL, n = 300, verbose = FALSE) {
+qGenWrap <- function(nX, nW, mu = NULL, n = 400, verbose = FALSE) {
   if (verbose) cat("n_X = ", nX, ", n_W = ", unlist(nW), ", mu = ",
                    ifelse(is.null(mu), 0, mu), "\n")
 	# ad-hoc wrapper for questionnaire_gen()
@@ -38,23 +38,14 @@ qGenWrap <- function(nX, nW, mu = NULL, n = 300, verbose = FALSE) {
 
 # The numbering logic below is df_abc, where the "abc" contains numbers of n_X amd n_W and for mu
 
-df_110b <- qGenWrap(1, list(2))
-df_229m <- qGenWrap(2, list(2, 3), 9)
-df_229nm <- qGenWrap(2, list(2, 3), -9)
 df_111mb <- qGenWrap(1, list(2), c(20, -30))
 
 # Checking means -------------------------------------------------------------
 test_that("Observed means equal expectations", {
-  expect_equivalent(df_110b$means, c(0, 0), tolerance = 0.2)
-  expect_equivalent(df_229m$means, rep(9, 3),  tolerance = 0.2)
-  expect_equivalent(df_229nm$means, rep(-9, 3), tolerance = 0.2)
   expect_equivalent(df_111mb$means, c(20, -30),  tolerance = 0.2)
 })
 
 # Checking betas --------------------------------------------------------------
 test_that("Estimated true betas are equivalent to MC estimates", {
-  expect_gte(min(df_110b$avg_coverage), .5)
-  expect_gte(min(df_229m$avg_coverage), .5)
-  expect_gte(min(df_229nm$avg_coverage), .5)
   expect_gte(min(df_111mb$avg_coverage), .5)
 })
--- tests/testthat/test.betagenCsd.R
+++ tests/testthat/test.betagenCsd.R
@@ -1,7 +1,7 @@
 context("beta_gen() behavior with n_W > 0 and c_sd != 1")
 
 # Generating data -------------------------------------------------------------
-qGenWrap <- function(nX, nW, sd = NULL, n = 300, verbose = FALSE) {
+qGenWrap <- function(nX, nW, sd = NULL, n = 400, verbose = FALSE) {
   if (verbose) cat("n_X = ", nX, ", n_W = ", unlist(nW), ", sd = ",
                    ifelse(is.null(sd), 0, sd), "\n")
 	# ad-hoc wrapper for questionnaire_gen()
@@ -22,7 +22,7 @@ qGenWrap <- function(nX, nW, sd = NULL, n = 300, verbose = FALSE) {
   
   # Constructing a coverage percentage for the true betas
   betas <- list()
-  for (rep in seq(100)) {
+  for (rep in seq(50)) {
     if (verbose & (rep %% 5 == 0)) cat(rep, "")
     betas[[rep]] <- beta_gen(data    = data,
                              MC      = TRUE,
@@ -38,23 +38,14 @@ qGenWrap <- function(nX, nW, sd = NULL, n = 300, verbose = FALSE) {
 
 # The numbering logic below is df_abc, where the "abc" contains numbers for n_X, n_W and sd
 
-df_101  <- qGenWrap(1, 0, 2:3)
-df_011t <- qGenWrap(0, list(3), 2)
-df_111t <- qGenWrap(1, list(3), 2:3)
 df_229m <- qGenWrap(2, list(2, 3), 7:9)
 
 # Checking means -------------------------------------------------------------
 test_that("Observed means equal expectations", {
-  expect_equivalent(df_101$sds,  2:3, tolerance = 0.2)
-  expect_equivalent(df_011t$sds, 2,   tolerance = 0.2)
-  expect_equivalent(df_111t$sds, 2:3, tolerance = 0.2)
   expect_equivalent(df_229m$sds, 7:9, tolerance = 0.2)
 })
 
 # Checking betas --------------------------------------------------------------
 test_that("Estimated true betas are equivalent to MC estimates", {
-  expect_gte(min(df_101$avg_coverage), .5)
-  expect_gte(min(df_011t$avg_coverage), .5)
-  expect_gte(min(df_111t$avg_coverage), .5)
   expect_gte(min(df_229m$avg_coverage), .5)
 })
