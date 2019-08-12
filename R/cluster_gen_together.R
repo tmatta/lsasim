b5d9e6416c7aba490c52cb1e9fd30c3a72a79742
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-12 19:21:41 +0200

    Sequential numbering of students

--- R/cluster_gen_together.R
+++ R/cluster_gen_together.R
@@ -37,6 +37,9 @@ cluster_gen_together <- function(n_levels, c_mean_list, clusters, n_obs, cluster
         # Saving the questionnaire to the final list (sample)
         cluster_bg -> sample[[c]]
       }
-      if (collapse) sample <- do.call(rbind, sample)
+      if (collapse) {
+        sample <- do.call(rbind, sample)
+        sample$subject <- seq(nrow(sample))
+      }
 	  return(sample)
 }
