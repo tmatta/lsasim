4a5c61a95f5c5ffd969d09c42f0edf51aa5a377e
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-19 11:57:14 +0200

    Fixed ID numbers for !separate_questionnaires

--- R/cluster_gen_together.R
+++ R/cluster_gen_together.R
@@ -17,12 +17,13 @@ cluster_gen_together <- function(n_levels, clusters, n_obs,
   for (c in 1:n_levels) {
     level_combos[[n_levels - c + 1]] <- seq(from = 1, to = clusters[c])
   }
+
   id_combos <- expand.grid(level_combos)
   id_combos <- id_combos[, ncol(id_combos):1]  # so first level comes first
   colnames(id_combos) <- labels
 
   for (c in seq(ncol(id_combos))) {
-    id_combos[, c] <- paste0(labels[c])
+    id_combos[, c] <- paste0(labels[c], id_combos[, c])
   }
 
   num_questionnaires <- nrow(id_combos)
@@ -36,11 +37,6 @@ cluster_gen_together <- function(n_levels, clusters, n_obs,
       studentID <- paste0("student", seq(nrow(cluster_bg)))
       clusterID <- paste(rev(id_combos[c, ]), collapse = "_")
       cluster_bg$uniqueID <- paste(studentID, clusterID, sep = "_")
-
-      # Relabeling the subjects
-      # last_subject <- n_obs[l] * c
-      # first_subject <- last_subject - n_obs[l] + 1
-      # cluster_bg$subject <- first_subject:last_subject
 
       # Saving the questionnaire to the final list (sample)
       cluster_bg -> sample[[c]]
