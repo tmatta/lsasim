b52d41680911759bdd18d76fe25ae4afa09db839
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-20 16:02:13 +0200

    Fixed subject labeling for collapse

--- R/cluster_gen_separate.R
+++ R/cluster_gen_separate.R
@@ -121,7 +121,7 @@ cluster_gen_separate <- function(n_levels, n, N, sum_pop,  calc_weights,
           out[paste0("clusterID.", resp_labels[1])] <- NULL
           out[paste0("clusterID.", resp_labels[l])] <- NULL
           # Renaming subjects (variable and values)
-          names(out)[1] <- resp_labels[l]
+          names(out)[1] <- "subject"
           out$subject <- seq(nrow(out))
         }
       } else {
