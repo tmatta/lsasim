d8f62370a9a2a907f6b1ad103a184b42ecf724ec
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-22 18:32:41 +0200

    Proper calculation of total respondents

--- R/cluster_gen_auxiliaryFunctions.R
+++ R/cluster_gen_auxiliaryFunctions.R
@@ -1,48 +1,67 @@
-clusterMessage <- function(n_obs, resp_labels, cluster_labels, n_levels, type)
-{
+clusterMessage <- function(n_obs, resp_labels, cluster_labels, n_levels,
+                           separate_questionnaires, type) {
   # This function prints messages about the cluster scheme before generating questionnaire responses. All arguments are from cluster_gen except for "type", which is numeric and changes the way the first line is printed.
-  if (type == 1)
-  {
+  if (type == 1) {
     # Comma-separated multiple questionnaires
     message("Generating questionnaires for ",
             paste(cluster_labels, collapse = ", "))
-  }
-  else
-  {
+  } else {
     # Questionnaires only for the lowest level
     message("Generating questionnaires for ", resp_labels[n_levels - 1])  
   }
-  for (l in 1:(length(n_obs) - 1)) # Final row of messages is different
-  {
+
+  tot_resp <- 0
+  operands <- NULL
+  for (l in seq(length(n_obs) - 1)) { # Final row of messages is different
     n_obs_print <- n_obs
-    if (class(n_obs) == "list")
-    {
-      n_obs_print[[l]] <- paste("between", n_obs[[l]][1], "and", n_obs[[l]][2])
-      n_obs_print[[l + 1]] <- paste("between", n_obs[[l + 1]][1], "and", n_obs[[l + 1]][2])
+
+    # Printing top level
+    if (l == 1) {
+      message("Top level: ", cluster_labels[l], " (", n_obs_print[l], ")")
     }
-    if (l == 1) message("Top level: ", n_obs_print[l], " ", cluster_labels[l])
-    if (l < length(n_obs) - 1)
-    {
-      message("Each ", cluster_labels[l], " sampled ", n_obs_print[l + 1], " ",
-              cluster_labels[l + 1])
+
+    # Printing second to second-to-last levels
+    if (class(n_obs) == "list" & length(n_obs[[l + 1]]) > 1) {
+      n_obs_print[[l]] <- paste0(paste(n_obs[[l]], collapse = " and "),
+                                 ", respectively")
+      n_obs_print[[l + 1]] <- paste0(paste(n_obs[[l + 1]], collapse = " and "),
+                                     ", respectively")
+    }
+    if (l < length(n_obs) - 1) {
+      message("Each ", cluster_labels[l], " sampled ",  cluster_labels[l + 1],
+              " (", n_obs_print[l + 1], ")")
+    }
+    if (l > 1 & class(n_obs) != "list" & separate_questionnaires) {
+      tot_resp <- tot_resp + prod(n_obs[1:l])
+      operands <- c(operands, prod(n_obs[1:l]))
     }
   }
+
   # Final row of messages
   message("Each ", cluster_labels[n_levels - 1], " sampled ",
-          n_obs_print[n_levels], " ", resp_labels[n_levels - 1])
+          resp_labels[n_levels - 1], " (", n_obs_print[n_levels], ")")
 
-  if (class(n_obs) == "list")
-  {
-    min_obs <- sapply(n_obs, function(x) x[1])
-    max_obs <- sapply(n_obs, function(x) x[length(x)])
-      message("Estimated total respondents: between ",
-          paste0(prod(min_obs), " (",
-                 paste(min_obs, collapse = " * "), ") and ",
-                 prod(max_obs), " (", paste(max_obs, collapse = " * "), ")"))
+  if (class(n_obs) == "list") {
+    if (separate_questionnaires) {
+      tot_resp <- sum(unlist(n_obs))
+      operands <- unlist(n_obs)
+      operator <- " + "
+    } else {
+      tot_resp <- sum(unlist(n_obs[[n_levels]]))
+      operands <- unlist(n_obs[[n_levels]])
+      operator <- " + "
+    }
+  } else {
+    if (separate_questionnaires) {
+      tot_resp <- tot_resp + prod(n_obs)
+      operands <- c(operands, prod(n_obs))
+      operator <-  " + "
+    } else {
+      tot_resp <- prod(n_obs)
+      operands <- n_obs
+      operator <- " * "
+    }
   }
-  else
-  {
-    message("Total respondents: ", paste0(prod(unlist(n_obs)), " (",
-            paste(unlist(n_obs), collapse = " * "), ")"))
-  }
+  message("Total respondents: ", paste0(tot_resp, " (",
+            paste(operands, collapse = operator), ")"))
 }
