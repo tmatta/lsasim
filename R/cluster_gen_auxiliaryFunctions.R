d8ba7dc03e4f832e73469f4fbff115962fdae4e1
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-08-26 12:44:02 +0200

    Added list support for separate_questionnaires

--- R/cluster_gen_auxiliaryFunctions.R
+++ R/cluster_gen_auxiliaryFunctions.R
@@ -66,3 +66,46 @@ clusterMessage <- function(n_obs, resp_labels, cluster_labels, n_levels,
   message("Total respondents: ", paste0(tot_resp, " (",
             paste(operands, collapse = operator), ")"))
 }
+
+labelRespondents <- function (n_obs, cluster_labels) {
+  # This function nerated level label combinations for each respondent
+
+  n_levels <- length(n_obs)
+	level_combos <- list()  # will store ID combinations
+
+  if (class(n_obs) == "list") {
+    n_combos <- sum(n_obs[[n_levels - 1]])
+    second_last_level <- unlist(sapply(n_obs[[n_levels - 1]], seq))
+    id_combos <- matrix(second_last_level, ncol = n_combos)
+    for (row in 2:(n_levels - 1)) {
+      id_level <- n_levels - row
+      if (length(n_obs[[id_level]]) > 1 & all(n_obs[[id_level]] == 1)) {
+        n_obs[[id_level]] <- sum(n_obs[[id_level]])
+      }
+      expanded_level <- as.vector(unlist(sapply(n_obs[[id_level]], seq)))
+      expanded_level_col <- 1
+      new_row <- matrix(ncol = n_combos)
+      for (col in seq(n_combos)) {
+        if (all(id_combos[, col] == 1)) {
+          new_row[col] <- expanded_level[expanded_level_col]
+          expanded_level_col <- expanded_level_col + 1
+        } else {
+          new_row[col] <- new_row[col - 1]
+        }
+      }
+      id_combos <- rbind(id_combos, new_row)
+    }
+    id_combos <- t(id_combos)
+  } else {
+    for (l in 1:(n_levels - 1)) {
+      level_combos[[n_levels - l]] <- seq(from = 1, to = n_obs[l])
+    }
+    id_combos <- expand.grid(level_combos)
+  }
+  id_combos <- id_combos[, ncol(id_combos):1]  # so first level comes first
+  colnames(id_combos) <- cluster_labels[-n_levels]
+  for (l in seq(ncol(id_combos))) {
+    id_combos[, l] <- paste0(cluster_labels[l], id_combos[, l])
+  }
+  return(id_combos)
+}
