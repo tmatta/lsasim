381869c959b9b85fbc510445c935b59c33dd904a
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-09-06 15:30:21 +0200

    Added reference to welcome function (from lavaan)

--- R/misc.R
+++ R/misc.R
@@ -1,7 +1,12 @@
-# Contains miscellaneous functions that are not to be used by a package user.
+# This file contains miscellaneous functions that are not to be used by a package user.
 
+#' @title Prints welcome message on package load
+#' @description Prints "This is lsasim <version number>" on package load
+#' @note This function was adapted from the lavaan package, so credit for it goes to lavaan's creator, Yves Rosseel
+#' @references Yves Rosseel (2012). lavaan: An R Package for Structural Equation
+#'  Modeling. Journal of Statistical Software, 48(2), 1-36. URL
+#'  http://www.jstatsoft.org/v48/i02/.
 .onAttach <- function(libname, pkgname) {
-    # Prints "This is lsasim <version number>" on package load
     version <- read.dcf(file   = system.file("DESCRIPTION", package = pkgname),
                         fields = "Version")
     packageStartupMessage("This is ", paste(pkgname, version))
