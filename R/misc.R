40afb583f89374897435ed1f09ddc217778dd5e9
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-09-06 18:14:01 +0200

    Added documentation to functions

--- R/misc.R
+++ R/misc.R
@@ -1,6 +1,8 @@
 # This file contains miscellaneous functions that are not to be used by a package user.
 
 #' @title Prints welcome message on package load
+#' @param libname no idea, but will break devtools::document() if removed
+#' @param pkgname name of the package
 #' @description Prints "This is lsasim <version number>" on package load
 #' @note This function was adapted from the lavaan package, so credit for it goes to lavaan's creator, Yves Rosseel
 #' @references Yves Rosseel (2012). lavaan: An R Package for Structural Equation
