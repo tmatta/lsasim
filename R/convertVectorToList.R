5a1b076ced02dcd339fb25387a8f148641b98b91
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-09-13 14:39:02 +0200

    Improved implementation of ranges()

--- R/convertVectorToList.R
+++ R/convertVectorToList.R
@@ -7,21 +7,11 @@ convertVectorToList <- function(x)
   x_list <- as.list(x)
   for (lvl in 2:(length(x))) {
     if (class(x_list[[lvl]]) == "range") {
-      browser()#TEMP
+      x_list[[lvl]] <- sample_within_range(x[[lvl]], sum(x_list[[lvl - 1]]))
     } else {
-      # browser()#TEMP
       x_list[[lvl]] <- rep(x[[lvl]], sum(x_list[[lvl - 1]]))
-                          # prod(seq(from = x[[1]],
-                          #           to   = x[[lvl - 1]],
-                          #           length.out = lvl - 1)))
     }
   }
   x <- x_list
   return(x)
 }
-
-
-# TODO: Testing. Remove when OK
-# n <- c(city = 2, school = 2, class = 3, student = 4)
-# n_list <- convertVectorToList(n)
-# drawClusterStructure(n_list)
