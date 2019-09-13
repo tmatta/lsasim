d589439f4b9eeec4372e38cc27092aab34736da7
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-09-13 16:44:34 +0200

    Todo list cleanup

--- tests/testthat/test.replicateWeights.R
+++
@@ -1,7 +0,0 @@
-# context("Basic summary statistics for cluster samples")
-
-# set.seed(230)
-# df <- cluster_gen(c(sch = 4, stu = 10), n_X = 3, n_W = 1, verb = TRUE)
-# jackknife(df$sch[[4]], c("sch.weight", "final.stu.weight"))
-# df2 <- cluster_estimates(df, var_method = "Jackknife", verbose = FALSE, print_data = TRUE)
-# ASK: Rename to "replicate_weights" and have the statistics be secondary?
