32d7b176bf3e29bf9010a0ab379596a73de3269f
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-09-13 10:27:26 +0200

    Added function for unwrapping range object

---
+++ R/sample_within_range.R
@@ -0,0 +1,17 @@
+#' @title Sample from range
+#' @description Creates a uniformly-distributed sample from a 2-length vector
+#' @note This function was created primarily to be used to expand an object with the "range" class.
+#' @param rg a "range"-class vector
+#' @param sample_size the size of the sample to be generated
+#' @return A vector containing the generated sample
+sample_within_range <- function(rg, sample_size = NULL)
+{
+    check_condition(class(rg) != "range" | length(rg) != 2,
+                    "Wrong class or size for rg object")
+    minimum <- rg[1]
+    maximum <- rg[2]
+    sample_space <- seq(from = minimum, to = maximum)
+    if (is.null(sample_size)) sample_size <- length(sample_space)
+    out <- sample(sample_space, sample_size, replace = TRUE)
+    return(out)
+}
