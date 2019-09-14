3f2e8c36a672b87ba788d7461e50a8837f0242b0
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-09-14 10:15:07 +0200

    Bottom-up sampling (bad idea)

--- R/sample_from.R
+++ R/sample_from.R
@@ -1,6 +1,7 @@
 #' @title Sample from population structure
 #' @description Generates a sample from a population structure
-sample_from <- function (N, n, labels = names(N))
+#' @param N list containing the population sampling structure
+sample_from <- function (N, n, labels = names(N), verbose = TRUE)
 {
   # Creating basic elements ====================================================
   if (class(N) != "list") N <- convert_vector_to_list(N)
@@ -15,9 +16,33 @@ sample_from <- function (N, n, labels = names(N))
     sampled_units <- sampled_units[selected_rows, , drop = FALSE]
   }
 
-  # Sampling from last level ===================================================
-  browser()#TEMP
+  # Sampling setup =============================================================
+  # TODO: sample from top to bottom
+  # TODO: start by selecting elements_1st_lvl
+  # TODO: then select second level based on results from 1st. Then rinse and repeat until last level.
   # lapply(sampled_units, 1, function(x) sample(n[length(n))
-  draw_cluster_structure(N, labels)
-  draw_cluster_structure(N, labels, output = "text")
+  if (verbose) message("Sampling from population")
+  elements <- draw_cluster_structure(N, labels, output = "text")
+
+  # Sampling from the last level ===============================================
+  sampled_elements <- 
+
+  # Retrieving elements from the last level ====================================
+  elements_last_lvl <- NULL
+  for (i in seq_along(elements)) {
+    is_last_lvl <- grepl("\\(", elements[i])
+    elements_last_lvl <- append(elements_last_lvl, is_last_lvl)
+  }
+  elements_last_lvl <- elements[elements_last_lvl]
+
+  # Cleaning up elements from last level =======================================
+  elements_last_lvl <- gsub("\\s.+", "", elements_last_lvl)
+  elements_last_lvl <- gsub("\\W", "", elements_last_lvl)
+
+  # Sampling from second-to-last level =========================================
+  sampled_elements <- append(sample(x = elements_last_lvl,
+                                    size = prod(n[1]:n[length(n) - 1])),
+                             sampled_elements)
+
+  browser()#TEMP
 }
