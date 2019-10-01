085d5a8b548c48943e4276a6ece35cd06bb4edda
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-10-01 18:23:03 +0200

    Fixed replicate weight calculation

--- R/replicate_var.R
+++ R/replicate_var.R
@@ -5,16 +5,34 @@
 #' @param method replication method. Can be "Jackknife", "BRR" or "BRR Fay"
 #' @param k deflating weight factor (used only when `method = "BRR Fay")
 #' @param stat statistic of interest to calculate (must be a base R function)
-#' @param theta vector containing the variables of interest
+#' @param vars vector containing the variables of interest
 #' @param full_output if `TRUE`, returns all intermediate objects created
 #' @details `data_rep` can be obtained from 
 #' @seealso jackknife brr
 #' @export
-replicate_var <- function(data_whole, data_rep, method, k = .5, stat = mean,
-                          theta = NULL, full_output = FALSE)
-{
+replicate_var <- function(data_whole, data_rep, method, k = .5,
+                          weight_var = NULL,
+                          stat = weighted.mean, vars = NULL,
+                          full_output = FALSE) {
     # Verification =============================================================
-    if (k < 0 | k > 1) stop ("k must be between 0 and 1")
+    if (k < 0 | k > 1) stop("k must be between 0 and 1")
+    if (method == "BRR" & k != .5 & k != 0) {
+        warning("BRR ignores k. Use 'BRR Fay' instead.")
+    }
+    if (missing(data_rep)) {
+        message("Generating replications and statistics with ", method)
+        if (method %in% c("Jackknife", "jackknife")) {
+            data_rep <- jackknife(data_whole, drop = FALSE)
+        } else if (method == "BRR") {
+            data_rep <- brr(data_whole, k = 0, drop = FALSE)
+        } else if (method == "BRR Fay") {
+            data_rep <- brr(data_whole, k = k, drop = FALSE)
+        } else {
+            stop("Invalid method. Please use 'Jackknife', 'BRR' or 'BRR Fay'.")
+        }
+        weight_var <- "replicate_weight"
+    }
+
 
     # Fetching statistic of interest ===========================================
     statistic <- match.fun(stat)
@@ -26,45 +44,66 @@ replicate_var <- function(data_whole, data_rep, method, k = .5, stat = mean,
     }
 
     # Determining which variables to use =======================================
-    if (is.null(theta)) theta <- names(data_whole)  # uses all variables
+    if (is.null(vars)) {
+        # use all variables
+        vars <- list(whole = names(data_whole), rep = names(data_rep[[1]]))
+    } else {
+        vars <- list(whole = vars, rep = c(vars, weight_var))
+    }
     
     # Dropping categorical and nominal variables ===============================
-    theta_classes <- sapply(data_whole[theta], class)
-    numeric_cols <- theta[grepl("numeric|integer", theta_classes)]
-    numeric_data_whole <- data_whole[numeric_cols]
-    numeric_data_rep <- lapply(data_rep, function(x) x[names(numeric_data_whole)])
+    vars_classes <- list(whole = sapply(data_whole[vars$whole], class),
+                          rep   = sapply(data_rep[[1]][vars$rep], class))
+    numeric_cols <- list(
+        whole = vars$whole[grepl("numeric|integer", vars_classes$whole)],
+        rep   = vars$rep[grepl("numeric|integer", vars_classes$rep)]
+    )
+    numeric_data <- list(
+        whole = data_whole[numeric_cols$whole],
+        rep   = lapply(data_rep, function(x) x[numeric_cols$rep])
+    )
 
-    # Calculating theta for the whole dataset and for each replication
-    theta_whole <- sapply(numeric_data_whole, statistic)
-    theta_rep <- t(sapply(numeric_data_rep, function(x) apply(x, 2, statistic)))
+    # Calculating theta for the whole dataset and for each replication =========
+    theta_whole <- sapply(numeric_data$whole, statistic)
+    if (is.null(weight_var)) {
+        statistic_rep <- function(x) apply(x, 2, statistic)
+    } else {
+        statistic_rep <- function(x, w = weight_var) {
+            x_without_w <- x[, -match(w, names(x))]
+            w_col <- x[, w]
+            apply(x_without_w, 2, function(x) statistic(x, w_col))
+        }
+    }
+    theta_rep <- t(sapply(numeric_data$rep, statistic_rep))
 
-    # Calculating mean variance ================================================
-    G <- length(theta_rep)
+    # Defining variance multiplier =============================================
+    G <- nrow(theta_rep)  # number of reps
+    if (method %in% c("Jackknife", "jackknife")) {
+        multiplier <- (G - 1) / G
+    } else if (method == "BRR") {
+        multiplier <- 1 / G
+    } else if (method == "BRR Fay") {
+        multiplier <- 1 / (G * (1 - k) ^ 2)
+    } else {
+        stop("Invalid method. Please use 'Jackknife', 'BRR' or 'BRR Fay'.")
+    }
+
+    # Looping through variables to calculate mean variances ====================
     sigma2 <- vector()
-    for (col in numeric_cols) {
+    for (col in numeric_cols$whole) {
 
         # Retrieving theta_i from theta_rep ------------------------------------
-        if (nrow(theta_rep) == 1) {
+        if (G == 1) {
             theta_i <- theta_rep
         } else {
             theta_i <- theta_rep[, col]
         }
 
-        # Defining variance multiplier -----------------------------------------
-        if (method %in% c("Jackknife", "jackknife")) {
-            multiplier <- (G - 1) / G
-        } else if (method == "BRR") {
-            multiplier <- 1 / G
-        } else if (method == "BRR Fay") {
-            multiplier <- 1 / (G * (1 - k) ^ 2)
-        } else {
-            stop("Invalid method. Please use 'Jackknife', 'BRR' or 'BRR Fay'.")
-        }
-
-        # Calculating variance and adding to output object ---------------------
+        # Calculating variance for this variable, adding to output object ------
         new_s2 <- multiplier * sum((theta_i - theta_whole[col]) ^ 2)
         sigma2 <- c(sigma2, new_s2)
     }
+    names(sigma2) <- numeric_cols$whole
 
     # Building output ==========================================================
     if (full_output) {
