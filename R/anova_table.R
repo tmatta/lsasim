025b4ca354d291ac7d66e952a472d8556f74e47d
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-11-04 14:10:34 +0100

    Bug fix for levels with only one member

--- R/anova_table.R
+++ R/anova_table.R
@@ -18,43 +18,47 @@ anova_table <- function(data, print = TRUE) {
     out_complete <- list()
     for (n in names(data)) {
         ds <- data_summary[[n]]
+        if (ds$N == 1) {
+            message("There is only one group of ", pluralize(n),
+                    ". Skipping ANOVA for that level.")
+        } else {
+            ## Sample statistics -----------------------------------------------
+            s2_within <- calc_var_within(ds$n_j, ds$s2_j, ds$M, ds$N)
+            s2_between <- calc_var_between(ds$n_j, ds$y_bar_j, ds$y_bar, ds$n_tilde, ds$N)
+            s2_total <- calc_var_tot(ds$M, ds$N, ds$n_tilde, s2_within, s2_between)
+            out <- list(sample_statistics = c(s2_within = s2_within,
+                                            s2_between = s2_between,
+                                            s2_total = s2_total))
 
-        ## Sample statistics ---------------------------------------------------
-        s2_within <- calc_var_within(ds$n_j, ds$s2_j, ds$M, ds$N)
-        s2_between <- calc_var_between(ds$n_j, ds$y_bar_j, ds$y_bar, ds$n_tilde, ds$N)
-        s2_total <- calc_var_tot(ds$M, ds$N, ds$n_tilde, s2_within, s2_between)
-        out <- list(sample_statistics = c(s2_within = s2_within,
-                                          s2_between = s2_between,
-                                          s2_total = s2_total))
+            ## Population parameters and ANOVA table ---------------------------
+            sigma2_hat <- s2_within
+            X <- names(sigma2_hat)
+            for (x in X) {
+                tau2_hat <- max(s2_between[x] - s2_within[x] / ds$n_tilde, 0)
+                rho_hat <-  intraclass_cor(tau2_hat, sigma2_hat[x])
+                if (stats::var(ds$n_j) == 0) {
+                    se_rho <- calc_se_rho(rho_hat, ds$n_j, ds$N)
+                } else {
+                    se_rho <- NA
+                    warning("SE not yet implemented for different sample sizes")
+                }
+                out$population_estimates[[x]] <- c(sigma2_hat = sigma2_hat[x], 
+                                                tau2_hat = tau2_hat,
+                                                rho_hat = rho_hat,
+                                                se_rho = se_rho)
 
-        ## Population parameters and ANOVA table -------------------------------
-        sigma2_hat <- s2_within
-        X <- names(sigma2_hat)
-        for (x in X) {
-            tau2_hat <- max(s2_between[x] - s2_within[x] / ds$n_tilde, 0)
-            rho_hat <-  intraclass_cor(tau2_hat, sigma2_hat[x])
-            if (stats::var(ds$n_j) == 0) {
-                se_rho <- calc_se_rho(rho_hat, ds$n_j, ds$N)
-            } else {
-                se_rho <- NA
-                warning("SE not yet implemented for different sample sizes")
+                ### ANOVA table ................................................
+                if (print) {
+                    message("\nANOVA table for ", pluralize(n), ", ", x)
+                    print_anova_table(s2_within[x], s2_between[x], s2_total[x], 
+                                    sigma2_hat[x], tau2_hat, rho_hat, se_rho, 
+                                    ds$n_tilde, ds$M, ds$N)
+                }
             }
-            out$population_estimates[[x]] <- c(sigma2_hat = sigma2_hat[x], 
-                                               tau2_hat = tau2_hat,
-                                               rho_hat = rho_hat,
-                                               se_rho = se_rho)
-
-            ### ANOVA table ...................................................,
-            if (print) {
-                message("\nANOVA table for ", pluralize(n), ", ", x)
-                print_anova_table(s2_within[x], s2_between[x], s2_total[x], 
-                                  sigma2_hat[x], tau2_hat, rho_hat, se_rho, 
-                                  ds$n_tilde, ds$M, ds$N)
-            }
+            if (print & sapply(data, class)[1] == "list" &
+                (n != names(data)[length(names(data))])) cli::cat_rule()
+            out_complete[[n]] <- out
         }
-        if (print & sapply(data, class)[1] == "list" &
-            (n != names(data)[length(names(data))])) cli::cat_rule()
-        out_complete[[n]] <- out
     }
     if (!print) {
         if (length(out_complete) > 1) {
